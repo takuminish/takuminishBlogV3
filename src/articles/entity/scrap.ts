f945653f77153aa0eac517294996ba417c4b4a3c
takuminish <takuminv1997@gmail.com>
2025-10-28 22:30:58 +0900

    entityの作成

---
+++ src/articles/entity/scrap.ts
@@ -0,0 +1,12 @@
+export type ScrapDetail = {
+  slug: string;
+  title: string;
+  date: Date;
+  body: string;
+  linkedArticles: string[];
+};
+
+export type ScrapHead = {
+  slug: string;
+  title: string;
+};
